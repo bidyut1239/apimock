4f290a020c8114dad8311d1fde71164666651f3c
bidyut <bidyutssss@gmail.com>
2018-02-02 17:04:09 +0530

    ReadMe Updates

--- src/api/testapis/handlers.js
+++ src/api/testapis/handlers.js
@@ -1,11 +1,9 @@
 export let hello = (request, reply) => {
   let helloStr = "Hello, World";
-  console.log(`Hello`);
   reply(helloStr);
 };
 
 export let helloUnique = (request, reply) => {
   let helloStr = "Hello, " + encodeURIComponent(request.params.name) + "";
-  console.log(`Hits`);
   reply(helloStr);
 };
